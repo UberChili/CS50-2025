52490e4b091dc44363705c1850d89d5bbb07189e
Andrés <nunezlopez.ja@gmail.com>
2025-05-01 14:49:15 -0600

    Started work on readability

--- week2/pset2/readability/readability.c
+++ week2/pset2/readability/readability.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -32,8 +33,32 @@ char* get_string(char* prompt) {
     return result;
 }
 
+int calc_coleman_liau(char* text) {
+    int letters = 0;
+    int words = 0;
+
+    for (int i = 0; i < strlen(text); i++) {
+        if (!isalpha(text[i])) {
+            letters++;
+        } else if (text[i] == ' ') {
+            words++;
+        }
+    }
+
+    // calculate letters and sentences averages
+    float L = letters / 100.0;
+    float S = words / 100.0;
+
+    // return index
+    return (0.0588 * L) - (0.296 * S) - 15.8;
+}
+
 int main(void) {
 
+    char* text = get_string("Text: ");
+
+    int index = calc_coleman_liau(text);
+    printf("%d\n", index);
 
     return 0;
 }
