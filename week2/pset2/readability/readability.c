b7fc3b3365c30981c98856f1bf4e01ee1194f627
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-05-01 17:06:01 -0600

    Tired of readability. Let's move on

--- week2/pset2/readability/readability.c
+++ week2/pset2/readability/readability.c
@@ -1,11 +1,11 @@
 #include <ctype.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-char* get_string(char* prompt);
+char *get_string(char *prompt);
 
-char* get_string(char* prompt) {
+char *get_string(char *prompt) {
     printf("%s", prompt);
     char input[256];
 
@@ -21,44 +21,62 @@ char* get_string(char* prompt) {
     }
 
     // Allocating memory to return
-    char* result = malloc(strlen(input) + 1);
+    char *result = malloc(strlen(input) + 1);
     if (result == NULL) {
         printf("Error allocating memory for string\n");
         return NULL;
     }
 
-    // Copying string to be able to return a valid memory (and not something out of scope)
+    // Copying string to be able to return a valid memory (and not something out
+    // of scope)
     strcpy(result, input);
 
     return result;
 }
 
-int calc_coleman_liau(char* text) {
+int calc_coleman_liau(char *text) {
     int letters = 0;
     int words = 0;
+    int sentences = 0;
 
-    for (int i = 0; i < strlen(text); i++) {
-        if (!isalpha(text[i])) {
-            letters++;
-        } else if (text[i] == ' ') {
+    size_t len = strlen(text);
+
+    if (len > 0) {
+        words++;
+    }
+
+    for (int i = 0; i < len; i++) {
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!') {
+            sentences++;
+        } else if (isspace(text[i])) {
             words++;
+        } else if (isalpha(text[i])) {
+            letters++;
         }
     }
 
     // calculate letters and sentences averages
-    float L = letters / 100.0;
-    float S = words / 100.0;
+    float L = ((float)letters / words) * 100.0;
+    float S = ((float)sentences / words) * 100.0;
 
     // return index
-    return (0.0588 * L) - (0.296 * S) - 15.8;
+    return 0.0588 * L - 0.296 * S - 15.8;
 }
 
 int main(void) {
 
-    char* text = get_string("Text: ");
+    char *text = get_string("Text: ");
 
     int index = calc_coleman_liau(text);
-    printf("%d\n", index);
+    free(text);
+
+    if (index < 1) {
+        printf("Before Grade 1\n");
+    } else if (index > 15) {
+        printf("Grade %d+\n", index);
+    } else {
+        printf("Grade %d\n", index);
+    }
 
     return 0;
 }
