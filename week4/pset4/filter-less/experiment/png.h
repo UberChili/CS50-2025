7255552666d539fb1bca10ddc525ac33332f1069
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-05-23 19:56:01 -0600

    Changed structure. Created png.c

--- week4/pset4/filter-less/experiment/png.h
+++ week4/pset4/filter-less/experiment/png.h
@@ -1,30 +1,10 @@
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
-// PNG Signature. It is always the following values
-const uint8_t png_signature[] = {137, 80, 78, 71, 13, 10, 26, 10};
-
+// Struct for the Signature Header
 typedef struct {
     uint8_t values[8];
 } __attribute__((__packed__)) PNGSIGNATUREHEADER;
 
-bool valid_SH(PNGSIGNATUREHEADER *signature_header, FILE *file) {
-    if (fread(signature_header, sizeof(PNGSIGNATUREHEADER), 1, file) != 1) {
-        printf("Error: Couldn't read Signature Header.\n");
-        return false;
-    }
-
-    // Compare each element in the Signature Header
-    for (size_t i = 0, n = sizeof(signature_header->values) / sizeof(uint8_t);
-         i < n; i++) {
-        /* printf("%02x ", signature_header->values[i]); */
-        printf("%u ", signature_header->values[i]);
-        if (signature_header->values[i] != png_signature[i]) {
-            printf("Mismatch in Signature Header.\n");
-            return false;
-        }
-    }
-
-    return true;
-}
+// Checks for a valid Signature Header
+bool valid_SH(PNGSIGNATUREHEADER *signature_header, FILE *file);
