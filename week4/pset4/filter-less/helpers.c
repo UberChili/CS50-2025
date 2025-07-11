6750e1cba382379b4bc305577d7260f15e766c00
Andrés <nunezlopez.ja@gmail.com>
2025-07-11 13:33:06 -0600

    Compelted most filters. Working on Blur

--- week4/pset4/filter-less/helpers.c
+++ week4/pset4/filter-less/helpers.c
@@ -1,25 +1,82 @@
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+        }
+    }
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int sepiaRed = round((0.393 * image[i][j].rgbtRed) + (0.769 * image[i][j].rgbtGreen) + (0.189 * image[i][j].rgbtBlue));
+            if (sepiaRed > 255) {
+                sepiaRed = 255;
+            }
+            int sepiaGreen = round((0.349 * image[i][j].rgbtRed) + (0.686 * image[i][j].rgbtGreen) + (0.168 * image[i][j].rgbtBlue));
+            if (sepiaGreen > 255) {
+                sepiaGreen = 255;
+            }
+            int sepiaBlue = round((0.272 * image[i][j].rgbtRed) + (0.534 * image[i][j].rgbtGreen) + (0.131 * image[i][j].rgbtBlue));
+            if (sepiaBlue > 255) {
+                sepiaBlue = 255;
+            }
+
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++) {
+        for(int j = 0; j < width / 2; j++) {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - 1 - j];
+            image[i][width - 1 - j] = temp;
+        }
+    }
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    // Create a copy of image
+    RGBTRIPLE copy[height][width];
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            copy[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int redSum = 0, greenSum = 0, blueSum = 0;
+            int validPixelCount = 0;
+
+            for (int rowOffset = -1; rowOffset <= 1; rowOffset++) {
+                for (int colOffset = -1; colOffset <= 1; colOffset++) {
+                    int neighborRow = i + rowOffset;
+                    int neighborCol = j + colOffset;
+
+                    if (neighborRow >= 0 && neighborRow < height && neighborCol >= 0 && neighborCol < width) {
+
+                    }
+                }
+            }
+        }
+    }
 }
