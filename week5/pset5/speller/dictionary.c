ee5fe31968cd89fca98e3836ce5a989abf1a61c8
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-07-16 15:41:25 -0600

    Seemingly finished Speller. Need to test...

--- week5/pset5/speller/dictionary.c
+++ week5/pset5/speller/dictionary.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
@@ -36,7 +37,7 @@ bool add_word(node *dictionary[], node *word_node, size_t pos) {
     // If there were elements in "bucket" - Need to loop thorough them
     // Or should I just add them in the front?
     word_node->next = dictionary[pos]->next;
-    dictionary[pos]->next = word_node;
+    dictionary[pos] = word_node;
 
     return true;
 }
@@ -44,13 +45,33 @@ bool add_word(node *dictionary[], node *word_node, size_t pos) {
 // Returns true if word is in dictionary, else false
 bool check(const char *word) {
     // TODO
+    const unsigned int w_hash = hash(word);
+
+    if (table[w_hash] == NULL) {
+        return false;
+    }
+    node *ptr = table[w_hash];
+    while (ptr != NULL) {
+        if (strcasecmp(ptr->word, word) == 0) {
+            return true;
+        } else {
+            ptr = ptr->next;
+        }
+    }
+
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word) {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    /* return toupper(word[0]) - 'A'; */
+    // IDEA: Sum ASCII values, and take remainder of module number of buckets
+    int sum = 0;
+    for (int i = 0, w_len = strlen(word); i < w_len; i++) {
+        sum += word[i];
+    }
+    return sum % N;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
@@ -70,10 +91,10 @@ bool load(const char *dictionary) {
             word[len - 1] = '\0';
         }
 
-        // Converting to lowercase
+        // Converting to uppercase
         for (size_t i = 0; i < len; i++) {
             if (isalpha(word[i]))
-                word[i] = tolower(word[i]);
+                word[i] = toupper(word[i]);
         }
 
         // Creating node and initializing its word field with word
@@ -86,10 +107,24 @@ bool load(const char *dictionary) {
         strcpy(n->word, word);
 
         // Adding word to hash table
-        printf("Initial of word %s is: %c and its number is: %d\n", n->word,
-               n->word[0], n->word[0] - 'a');
+        /* printf("Initial of word %s is: %c and its hashed number would be:
+         * %d\n", */
+        /*        n->word, n->word[0], n->word[0] - 'A'); */
+
+        /* int calculated_hash = 0; */
+        /* for (size_t i = 0, word_len = strlen(word); i < word_len; i++) { */
+        /*     calculated_hash += word[i]; */
+        /* } */
+        /* /\* calculated_hash = calculated_hash / strlen(word) % N; *\/ */
+        /* calculated_hash = calculated_hash % N; */
+        /* printf("Initial of word %s is: %c and its hashed number would be:
+         * %d\n", */
+        /*        n->word, n->word[0], calculated_hash); */
 
         // Actually adding it
+        if (!add_word(table, n, hash(word))) {
+            return false;
+        }
 
         free(n);
     }
@@ -102,11 +137,36 @@ bool load(const char *dictionary) {
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void) {
     // TODO
-    return 0;
+    /* return 0; */
+    int count = 0;
+    for (size_t i = 0; i < N; i++) {
+        if (table[i] != NULL) {
+            node *ptr = table[i];
+            while (ptr != NULL) {
+                count++;
+                ptr = ptr->next;
+            }
+        } else {
+            continue;
+        }
+    }
+    return count;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void) {
     // TODO
-    return false;
+    for (size_t i = 0; i < N; i++) {
+        if (table[i] != NULL) {
+            node *ptr = table[i];
+            node *aux = ptr;
+            while (ptr->next != NULL) {
+                aux = ptr;
+                ptr = ptr->next;
+                free(aux);
+            }
+        }
+    }
+
+    return true;
 }
